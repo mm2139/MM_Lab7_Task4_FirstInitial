5ee0b9363984cf783a95f64dbf103c26f90d8d6c
mmaalin <maalin.622139@swl.k12.oh.us>
2024-10-17 08:23:48 -0400

    fin

--- src/FirstInitial.java
+++ src/FirstInitial.java
@@ -1,5 +1,12 @@
-public class Main {
+public class FirstInitial {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("***      ***");
+        System.out.println("****    ****");
+        System.out.println("************");
+        for (int i = 0; i <2; i++) {
+            System.out.println("***      ***");
+        }
+
     }
 }
