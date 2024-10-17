653ea0755810760ad8e8e01c368794acffe18297
mmaalin <maalin.622139@swl.k12.oh.us>
2024-10-17 08:46:25 -0400

    fin

--- src/FirstInitial.java
+++ src/FirstInitial.java
@@ -1,12 +1,15 @@
 public class FirstInitial {
     public static void main(String[] args) {
 
-        System.out.println("***      ***");
-        System.out.println("****    ****");
-        System.out.println("************");
-        for (int i = 0; i <2; i++) {
-            System.out.println("***      ***");
-        }
+        for (int i = 1; i <6; i++) {
+           if (i == 1 || i == 4 ||i == 5) {
+               System.out.println("***      ***");
+           } else if (i == 2) {
+               System.out.println("****    ****");
+           } else {
+               System.out.println("************");
+               }
+           }
 
     }
 }
